d9970617c713a7982d4d9b212309ecd85b0ae800
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2023-10-09 00:13:50 +0600

    nukelab Docker

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+# Copyright (c) NukeLab Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+# Use the Ubuntu 22.04 image as a base
+ARG BASE_IMAGE=ubuntu:22.04
+FROM $BASE_IMAGE
+
+# Install dependencies
+RUN apt-get update && \ 
+    apt-get install -y \
+    python3 \
+    python3-pip \
+    nodejs \
+    npm \
+    libssl-dev \
+    libcurl4-openssl-dev && \
+    apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# Install configurable-http-proxy
+RUN npm install -g configurable-http-proxy
+
+# Install JupyterHub and its dependencies
+RUN pip3 install jupyterhub pycurl
+
+# Install authenticators and spawners
+RUN pip3 install oauthenticator dockerspawner jupyterhub-nativeauthenticator
+
+# Copy nukelab into the image
+COPY nukelab.png ./nukelab.png
+
+# Copy the JupyterHub configuration file into the image
+COPY jupyterhub_config.py ./jupyterhub_config.py
+
+# Create the data directory for storage
+RUN mkdir /data
+
+# Start JupyterHub with the configuration file
+CMD ["jupyterhub"]
---
+++ jupyterhub_config.py
@@ -0,0 +1,47 @@
+# Copyright (c) NukeLab Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+# Import modules
+from dockerspawner import DockerSpawner
+from nativeauthenticator import NativeAuthenticator
+import os
+
+# Set the base URL
+c.JupyterHub.base_url = "/nukelab/"
+
+# Set the logo
+c.JupyterHub.logo_file = "nukelab.png"
+
+
+# Set the authenticator
+c.JupyterHub.authenticator_class = NativeAuthenticator
+c.GenericOAuthenticator.enable_auth_state = True
+c.NativeAuthenticator.create_system_users = True
+
+# Set the allowed users
+c.Authenticator.allowed_users = set()
+c.Authenticator.admin_users = {"tahmid"}
+c.NativeAuthenticator.open_signup = True
+
+
+# Set the timeout to 300 seconds
+c.Spawner.http_timeout = 300
+c.Spawner.start_timeout = 300
+
+# Set the log level
+c.JupyterHub.log_level = "INFO"
+
+# Set the hub IP
+c.JupyterHub.hub_ip = "0.0.0.0"
+
+# Set the spawner
+c.DockerSpawner.network_name = "nukelab"
+c.DockerSpawner.remove = True
+c.JupyterHub.spawner_class = DockerSpawner
+notebook_dir = os.environ.get("DOCKER_NOTEBOOK_DIR") or "/home/nukelab/"
+c.DockerSpawner.notebook_dir = notebook_dir
+c.DockerSpawner.volumes = {"jupyterhub-user-{username}": notebook_dir}
+c.DockerSpawner.image = "nukelab-spawner"
+
+# Set the database
+c.JupyterHub.db_url = "sqlite:///data/jupyterhub.sqlite"
