b2c7779da5db7f79ec07bd2bda587f5db5ab443b
Ahnaf Tahmid Chowdhury <tahmid@nukehub.org>
2025-03-12 02:23:52 +0600

    basic service worker

--- jupyterhub/service-worker.js
+++ jupyterhub/service-worker.js
@@ -1,46 +1,15 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("nukelab-cache-v1").then((cache) => {
+      return cache.addAll(["/", "/manifest.json", "/logo.svg", "/logo.png"]);
+    })
+  );
+});
 
-            importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
-            
-            const routing = workbox.routing;
-            const strategies = workbox.strategies;
-            
-            workbox.routing.registerRoute(
-            	/.(?:css|js|jsx|json)(?|$)/,
-            	new workbox.strategies.StaleWhileRevalidate({
-            		"cacheName": "assets",
-            		plugins: [
-            			new workbox.expiration.Plugin({
-            				maxEntries: 1000,
-            				maxAgeSeconds: 31536000
-            			})
-            		]
-            	})
-            );
-            
-            workbox.routing.registerRoute(
-            	/.(?:png|jpg|jpeg|gif|woff2)$/,
-            	new workbox.strategies.CacheFirst({
-            		"cacheName": "images",
-            		plugins: [
-            			new workbox.expiration.Plugin({
-            				maxEntries: 1000,
-            				maxAgeSeconds: 31536000
-            			})
-            		]
-            	})
-            );
-            
-            workbox.routing.registerRoute(
-            	/(\/)$/,
-            	new workbox.strategies.StaleWhileRevalidate({
-            		"cacheName": "startPage",
-            		plugins: [
-            			new workbox.expiration.Plugin({
-            				maxEntries: 1000,
-            				maxAgeSeconds: 31536000
-            			})
-            		]
-            	})
-            );
-
-        
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
